e67cb4645eb4a408e130ea5092913014ae32cd03
adambushman <adambushman@mail.weber.edu>
2022-04-19 07:29:06 -0600

    All setup for data import (will read right from depository)

--- store-sales-forecasting.py
+++ store-sales-forecasting.py
@@ -1,9 +1,11 @@
 import os
 import pandas as pd
 
-#os.chdir('C:/Users/Adam Bushman/Downloads')
-#os.chdir('C:/Users/Zach Manning/Downloads')
+# Change to your local directory
+#os.chdir('C:/Users/Adam Bushman/Downloads') 
 
 rawData = pd.read_csv('train-store-data.csv')
+print(rawData.head())
+print(rawData.info())
 
 #Successfully linked editor and GitHub! (Adam)
