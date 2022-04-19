9946d50566f79b94e09a0b3b5d89a8f02298bcfa
adambushman <adambushman@mail.weber.edu>
2022-04-19 07:26:00 -0600

    Successful link to editor (Adam)

--- store-sales-forecasting.py
+++ store-sales-forecasting.py
@@ -1,7 +1,9 @@
 import os
 import pandas as pd
 
-os.chdir('C:/Users/Adam Bushman/Downloads')
+#os.chdir('C:/Users/Adam Bushman/Downloads')
 #os.chdir('C:/Users/Zach Manning/Downloads')
 
 rawData = pd.read_csv('train-store-data.csv')
+
+#Successfully linked editor and GitHub! (Adam)
