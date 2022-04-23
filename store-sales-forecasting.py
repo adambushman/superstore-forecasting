68524a03eeb6c5c024bd7dc04c68b1332d23c05c
adambushman <adambushman@mail.weber.edu>
2022-04-23 11:47:34 -0600

    Final updates with prediction

--- store-sales-forecasting.py
+++ store-sales-forecasting.py
@@ -7,13 +7,15 @@ import numpy as np
 import math
 from statsmodels.tsa.stattools import adfuller
 from statsmodels.tsa.seasonal import seasonal_decompose
-from sklearn.feature_selection import RFECV
+from sklearn.feature_selection import RFE, RFECV
 from sklearn.linear_model import LinearRegression
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.neural_network import MLPRegressor
 from sklearn.preprocessing import StandardScaler
-from statsmodels.tsa.holtwinters import ExponentialSmoothing
+from statsmodels.tsa.holtwinters import SimpleExpSmoothing, ExponentialSmoothing
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from sklearn.model_selection import train_test_split
+import statsmodels.api as sma
 
 rawData = pd.read_csv('train-store-data.csv')
 
@@ -32,12 +34,14 @@ sns.set_theme(style="whitegrid", palette = sns.color_palette(colors))
 
 # Time Alterations
 rawData['orderYrMon'] = pd.to_datetime(rawData['Order Date']).dt.to_period('M').astype(str)
+rawData['orderYear'] = pd.to_datetime(rawData['Order Date']).dt.year.astype(int)
+rawData['orderQuarter'] = 'Q' + pd.to_datetime(rawData['Order Date']).dt.quarter.astype(str)
+rawData['orderMonth'] = 'M' + pd.to_datetime(rawData['Order Date']).dt.month.astype(str)
+rawData['logSales'] = np.log(rawData['Sales'])
+
 print(rawData['Order Date'].describe())
 print(rawData.info())
 print(rawData.head())
-
-# Log of Sales
-rawData['logSales'] = np.log(rawData['Sales'])
 
 # Forecast
 group_by_date_sales = rawData[['orderYrMon', 'Sales']].groupby(['orderYrMon']).sum().reset_index()
@@ -47,10 +51,15 @@ group_by_date_sales.index=pd.to_datetime(group_by_date_sales.index, format = '%Y
 group_by_date_sales.index.freq = 'MS'
 decompose_result = seasonal_decompose(group_by_date_sales, model='multiplicative')
 
+m = 12
+alpha = 1 / (2 * m)
+
 train, test = group_by_date_sales.iloc[:42, 0], group_by_date_sales.iloc[42:, 0]  #train set 42/48 = 87.5%
-model = ExponentialSmoothing(train, seasonal='mul',seasonal_periods=12, initialization_method='estimated').fit()
-pred = model.predict(start=test.index[0], end=test.index[-1])
-#predict (6 Months)
+modelS = SimpleExpSmoothing(train).fit(smoothing_level=alpha, optimized=False, use_brute=True)
+modelE = ExponentialSmoothing(train, seasonal='mul', seasonal_periods=12, initialization_method='estimated').fit()
+
+predS = modelS.predict(start=test.index[0], end=test.index[-1])
+predE = modelE.predict(start=test.index[0], end=test.index[-1])
 
 # Time-series plot
 ax = sns.lineplot(data = group_by_date_sales, x = 'orderYrMon', y = 'Sales', linewidth = 3)
@@ -66,7 +75,8 @@ plt.show()
 #Forcasting plot
 plt.plot(train.index, train, label='Train', linewidth = 3)
 plt.plot(test.index, test, label='Test', linewidth = 3)
-plt.plot(pred.index, pred, label='Holt-Winters', linewidth = 3)
+plt.plot(predE.index, predE, label='Holt-Winters', linewidth = 3)
+plt.plot(predS.index, predS, label='Simple Exp', linewidth = 3)
 plt.title("6 Month Sales Forecasting", fontsize = 20)
 plt.ylabel("Sales")
 plt.xticks(rotation=45)
@@ -74,55 +84,72 @@ plt.legend(loc='best')
 plt.show()
 
 #Forcasting Accuracy
-print(f'Mean Absolute Error = {mean_absolute_error(test,pred)}')
-print(f'Mean Squared Error = {mean_squared_error(test,pred)}')
-print(f'Root Mean Squared Error ={math.sqrt(mean_squared_error(test,pred))}')
-print(f'R^2 Score = {r2_score(test,pred)}\n')
+#print(f'R^2 [Holt-Winters] = {r2_score(test,predE)}')
+#print(f'R^2 [Simple Exp] = {r2_score(test,predS)}')
+print(f'Mean Squared Error [Holt-Winters] = {mean_squared_error(test,predE)}')
+print(f'Mean Squared Error [Simple Exp] = {mean_squared_error(test,predS)}')
+
 
 
 # Prediction
-predFeatures = ['Ship Mode', 'Order Date', 'Segment', 'Country', 'State', 'Region', 'Sub-Category', 'Category', 'logSales']
-predDF = rawData[predFeatures].copy(deep = True)
+predFeatures = ['Segment', 'Ship Mode', 'Region', 'Category', 'orderYear', 'orderMonth', 'orderQuarter', 'Sales']
+predMinus = ['Segment', 'Ship Mode', 'Region', 'Category', 'orderYear', 'orderMonth', 'orderQuarter']
+predDF = rawData[predFeatures].groupby(predMinus).sum('Sales').reset_index()
+predDF['logSales'] = np.log(predDF['Sales'])
 
 
 # Sales vs Shipping Mode
-sns.boxplot(x = 'Ship Mode', y = 'logSales', data = predDF).set_title(label = "Shipping Mode vs Sales [Log Scale]")
+sns.boxplot(x = 'Ship Mode', y = 'logSales', data = predDF).set_title(label = "Shipping Mode vs Monthly Sales [Log Scale]")
 plt.show()
 
 # Sales vs Segment
-sns.boxplot(x = 'Segment', y = 'logSales', data = predDF).set_title(label = "Customer Segment vs Sales [Log Scale]")
+sns.boxplot(x = 'Segment', y = 'logSales', data = predDF).set_title(label = "Customer Segment vs Monthly Sales [Log Scale]")
 plt.show()
 
 # Sales vs Category
-sns.boxplot(x = 'Category', y = 'logSales', data = predDF).set_title(label = "Product Category vs Sales [Log Scale]")
+sns.boxplot(x = 'Category', y = 'logSales', data = predDF).set_title(label = "Product Category vs Monthly Sales [Log Scale]")
 plt.show()
 
 # Sales vs Region
-sns.boxplot(x = 'Region', y = 'logSales', data = predDF).set_title(label = "Product Region vs Sales [Log Scale]")
+sns.boxplot(x = 'Region', y = 'logSales', data = predDF).set_title(label = "Product Region vs Monthly Sales [Log Scale]")
 plt.show()
 
 
 # RFE
 y = predDF['logSales']
-xDum = pd.concat([pd.get_dummies(predDF['Category']), pd.get_dummies(predDF['Ship Mode']), pd.get_dummies(predDF['Segment']), pd.get_dummies(predDF['Region'])], axis = 1)
-xLin = xDum.drop(columns=['Technology', 'Central', 'Consumer', 'First Class'])
-xScale =  pd.DataFrame(data = StandardScaler().fit_transform(xDum), columns = xDum.columns)
+xDum = dataDums = pd.concat([predDF['orderYear'], pd.get_dummies(predDF['Category']), pd.get_dummies(predDF['Ship Mode']), pd.get_dummies(predDF['Segment']), pd.get_dummies(predDF['Region']), pd.get_dummies(predDF['orderQuarter']), pd.get_dummies(predDF['orderMonth'])], axis = 1)
+xLin = xDum.drop(columns=['Technology', 'Central', 'Consumer', 'First Class', 'Q1', 'M1'])
 
 # Linear model
 linModel = LinearRegression()
 rfeLin = RFECV(linModel)
 rfeLinModel = rfeLin.fit(xLin, y)
-scoreLin = rfeLinModel.score(xLin, y)
-indxLin = [i for i, l in enumerate(list(rfeLinModel.support_)) if l == True]
-
 
 # Decision tree model
 dtModel = DecisionTreeRegressor()
 rfeDT = RFECV(dtModel)
 rfeDTModel = rfeDT.fit(xDum, y)
-scoreDT = rfeDTModel.score(xDum, y)
-indxDT = [i for i, l in enumerate(list(rfeDTModel.support_)) if l == True]
+
+print('The top Linear features are, in order:')
+linFeat = []
+for i in rfeLinModel.ranking_:
+    linFeat.append(xLin.columns[i])
+print(linFeat)
+print('The top Linear features are, in order:', rfeDTModel.ranking_)
+dtFeat = []
+for i in rfeDTModel.ranking_:
+    dtFeat.append(xDum.columns[i])
+print(dtFeat)
 
 
-print('Score:', round(scoreLin,3),'| The top Linear features are:', list(xLin.columns[indxLin]))
-print('Score:', round(scoreDT,3),'| The top Decision Tree features are:', list(xDum.columns[indxDT]))
+# Training, testing sets
+x = xLin[['Q3', 'Q4', 'West', 'East', 'Home Office', 'Corporate', 'Standard Class', 'Second Class', 'Office Supplies', 'orderYear']]
+x['const'] = x.apply(lambda x: 1, axis= 1)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.25, shuffle=False)
+
+
+# Linear Regression
+linModel = sma.OLS(y_train, x_train).fit()
+print(linModel.summary())
+yhat_lin = linModel.predict(x_test)
+print('R^2 of actual vs test:', r2_score(y_test, yhat_lin))
